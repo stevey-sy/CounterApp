30c4c712b5ab6102571c5e049dced92a4e2dda6d
stevey-sy <sinsy89@hanmail.net>
2021-03-25 18:19:51 +0900

    [EDIT] Saved State Module 적용

--- app/src/main/java/com/example/counterapp/MainViewModel.kt
+++ app/src/main/java/com/example/counterapp/MainViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.counterapp
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-
-    private var count = 0
+class MainViewModel(private val handle: SavedStateHandle) : ViewModel() {
+    // handle 사용
+    private var count = handle.get<Int>("count") ?: 0
         // setter 를 override 한다.
         // count 변수의 값이 바뀔 때마다
         // value 에 저장한다.
@@ -13,6 +14,7 @@ class MainViewModel : ViewModel() {
             countLiveData.value = value
             // 이 값을 확정 짓겠다.
             field = value
+            handle.set("count", value)
         }
 
     val countLiveData = MutableLiveData<Int>()
