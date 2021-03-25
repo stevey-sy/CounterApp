e2077b391961b9555dae92bb430464592b7d784c
stevey-sy <sinsy89@hanmail.net>
2021-03-25 15:35:00 +0900

    [EDIT] livedata 를 사용하여 코드 줄이기

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,73 +20,57 @@ class MainActivity : AppCompatActivity() {
     // 어떤 효과? viewModel 객체에 데이터를 담아 놓는 효과
     // 때문에 activity 가 어떤 시스템의 오류로 죽어버리면
     // 데이터도 같이 날아간다.
-    val viewModel by viewModels<MainViewModel>()
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var count2 = savedInstanceState?.getInt("count")
+
+        Log.d(TAG, "onSaveInstanceState: ${count2}")
+
         var tvCounter : TextView = findViewById(R.id.counter_text)
         var addButton : Button = findViewById(R.id.add_button)
         var subButton : Button = findViewById(R.id.sub_button)
 
-        tvCounter.text = "${viewModel.count}"
+//        tvCounter.text = "${viewModel.count}"
+
+        // livedata 를 사용하여 데이터의 변화를 관찰하도록 설정
+        // 어떤 데이터?
+        // view model 에서 사용하고 있는 데이터
+        viewModel.countLiveData.observe(this, Observer { count->
+            tvCounter.text = "$count"
+        })
 
         addButton.setOnClickListener {
-            viewModel.count++
-            tvCounter.text = "${viewModel.count}"
+//            viewModel.count++
+//            tvCounter.text = "${viewModel.count}"
+            viewModel.increaseCount()
         }
 
         subButton.setOnClickListener {
-            viewModel.count--
-            tvCounter.text = "${viewModel.count}"
+//            viewModel.count--
+//            tvCounter.text = "${viewModel.count}"
+            viewModel.decreaseCount()
         }
-
-        registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                Log.d(TAG, "onActivityCreated: ")
-
-            }
-
-            override fun onActivityStarted(activity: Activity) {
-
-            }
-
-            override fun onActivityResumed(activity: Activity) {
-
-            }
-
-            override fun onActivityPaused(activity: Activity) {
-
-            }
-
-            override fun onActivityStopped(activity: Activity) {
-
-            }
-
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-
-            }
-
-            override fun onActivityDestroyed(activity: Activity) {
-            }
-
-        })
 
     }
 
     // activity 가 종료될 때 데이터를 bundle 에 저장할 때 사용
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", viewModel.count)
-    }
-
-    // bundle 에 담긴 데이터를 사용해서 복원할 때 사용하는 메소드
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        viewModel.count = savedInstanceState.getInt("count")
-
-    }
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putInt("count", viewModel.count)
+//        Log.d(TAG, "onSaveInstanceState: ${viewModel.count}")
+//    }
+//
+//    // bundle 에 담긴 데이터를 사용해서 복원할 때 사용하는 메소드
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//        viewModel.count = savedInstanceState.getInt("count")
+//        Log.d(TAG, "onRestoreInstanceState: ${viewModel.count}")
+//
+//    }
 
 
 
--- app/src/main/java/com/example/counterapp/MainViewModel.kt
+++ app/src/main/java/com/example/counterapp/MainViewModel.kt
@@ -1,8 +1,30 @@
 package com.example.counterapp
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    var count = 0
+    private var count = 0
+        // setter 를 override 한다.
+        // count 변수의 값이 바뀔 때마다
+        // value 에 저장한다.
+        set(value) {
+            countLiveData.value = value
+            // 이 값을 확정 짓겠다.
+            field = value
+        }
+
+    val countLiveData = MutableLiveData<Int>()
+
+    // count 를 변경시킬 메소드
+    fun increaseCount() {
+        count++
+//        countLiveData.value = count
+    }
+
+    fun decreaseCount() {
+        count--
+//        countLiveData.value = count
+    }
 }
